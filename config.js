237972a4ecbce3d9ecc72d44884e34393afe0362
“Shweta <choti@ravikirans-air.lan>
2018-07-20 18:24:33 -0700

    committing config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://skoti:Archie123@ds143461.mlab.com:43461/favoriterecipes';
-exports.PORT = process.env.PORT || 8080;
+//exports.PORT = process.env.PORT || 8080;
 //exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_SECRET = 'favoriterecipes';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '365d';
